65c3a383fc756f0482f18f86bdb0252df4ebc2e8
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-02-07 00:07:34 +0600

    problem 02 some issue fixed

--- problem-02.js
+++ problem-02.js
@@ -4,15 +4,13 @@ function validOtp(otp) {
   }
   const absoluteAlphabet = otp.startsWith("ph-");
   const lengthOfAlphabet = otp.length === 8;
-  const bothConditions = absoluteAlphabet + lengthOfAlphabet;
-  if (bothConditions === true) {
+  if (absoluteAlphabet === true && lengthOfAlphabet === true) {
     return true;
   } else {
     return false;
   }
 }
-// console.log(validOtp("ph-12345"));
-// console.log(validOtp("otp-1234"));
-// console.log(validOtp("ph-12"));
-// console.log(validOtp(12345678));
 console.log(validOtp("ph-10985"));
+console.log(validOtp("otp-1234"));
+console.log(validOtp("ph-12"));
+console.log(validOtp(12345678));
