afdf39857bdfe9b7d23017eb0846fecd1a5ee34b
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-02-07 12:59:33 +0600

    p-03 final done

--- final.js
+++ final.js
@@ -22,6 +22,28 @@ function newPrice(currentPrice, discount) {
 //
 // problem 03 completed
 
+function finalScore(omr) {
+  // let omr = {
+  //   right: Number,
+  //   wrong: Number,
+  //   skip: Number,
+  // };
+
+  let right = omr.right;
+  let wrong = omr.wrong;
+  let skip = omr.skip;
+
+  if (right + wrong + skip !== 100) {
+    return "Invalid";
+  }
+
+  let rightAns = right * 1;
+  let wrongAns = wrong * 0.5;
+  let score = rightAns - wrongAns;
+
+  return Math.round(score);
+}
+
 //
 //
 //
