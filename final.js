0f8c0b9715d872ffda4ebeaf229ad612b44084c1
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-02-07 13:40:11 +0600

    problem 04 solved 100%

--- final.js
+++ final.js
@@ -60,6 +60,32 @@ function finalScore(omr) {
 //
 //
 // problem 04 completed
+
+function gonoVote(array) {
+  if (!Array.isArray(array)) {
+    return "Invalid";
+  }
+  let votYes = 0;
+  let votNo = 0;
+  for (let count = 0; count < array.length; count++) {
+    const vot = array[count];
+
+    if (vot === "ha") {
+      votYes = votYes + 1;
+    } else if (vot === "na") {
+      votNo = votNo + 1;
+    }
+    // else{return "Invalid"}
+  }
+  if (votYes > votNo) {
+    return true;
+  } else if (votYes === votNo) {
+    return "equal";
+  } else {
+    return false;
+  }
+}
+
 //
 //
 //
--- problem-04.js
+++ problem-04.js
@@ -1,4 +1,28 @@
-/*function signature/sample */
 function gonoVote(array) {
-  //write your code here
+  if (!Array.isArray(array)) {
+    return "Invalid";
+  }
+  let votYes = 0;
+  let votNo = 0;
+  for (let count = 0; count < array.length; count++) {
+    const vot = array[count];
+
+    if (vot === "ha") {
+      votYes = votYes + 1;
+    } else if (vot === "na") {
+      votNo = votNo + 1;
+    }
+    // else{return "Invalid"}
+  }
+  if (votYes > votNo) {
+    return true;
+  } else if (votYes === votNo) {
+    return "equal";
+  } else {
+    return false;
+  }
 }
+
+console.log(gonoVote(["ha", "na", "ha", "ha", "na", "ha"]));
+console.log(gonoVote(["ha", "na", "na", "ha", "ha", "na", "na", "ha"]));
+console.log(gonoVote(["na", "na", "ha", "na", "na", "ha"]));
