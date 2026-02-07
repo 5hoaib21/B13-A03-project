455bd8a48b5910e69e0f019dd95d3f06d0f6aa88
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-02-07 15:21:06 +0600

    problem 05 solved 100%

--- problem-05.js
+++ problem-05.js
@@ -1,4 +1,24 @@
-/*function signature/sample */
 function analyzeText(str) {
-  // You have to write your code here
+  if (typeof str !== "string" || str.trim().length === 0) {
+    return "Invalid";
+  }
+
+  const noSpaceWord = str.split(" ").join("");
+  const tokenCuntt = noSpaceWord.length;
+
+  const arrayWord = str.split(" ");
+  let longest = "";
+
+  for (let i = 0; i < arrayWord.length; i++) {
+    const currentWord = arrayWord[i];
+
+    if (currentWord.length > longest.length) {
+      longest = currentWord;
+    }
+  }
+
+  return {
+    longwords: longest,
+    token: tokenCuntt,
+  };
 }
